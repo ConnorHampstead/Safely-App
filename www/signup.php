074efb391379902ca365d9414f341e96e20a9e70
limchunkuan <47402721+limchunkuan@users.noreply.github.com>
2019-03-28 13:54:01 +0000

    created panic button and edited singup.php

--- www/signup.php
+++ www/signup.php
@@ -8,13 +8,13 @@ if (!$conn) {
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-	if ($_POST['Password'] == $_POST['Confirm Password']){
+	if ($_POST['Password'] == $_POST['Confirm_Password']){
 		
-		$email = $mysqli->real_escape_string($_POST['Email']);
-		$first_name = $mysqli->real_escape_string($_POST['firstName']);
-		$last_name = $mysqli->real_escape_string($_POST['lastName']);
-		$phone_num = $mysqli->real_escape_string($_POST['Phone']);
-		$password = $mysqli->real_escape_string($_POST['Password']);
+		$email = $conn->real_escape_string($_POST['Email']);
+		$first_name = isset($_POST['firstName']) ? $conn->real_escape_string($_POST['firstName']) : '';
+		$last_name = isset($_POST['lastName']) ? $conn->real_escape_string($_POST['lastName']): '';
+		$phone_num = $conn->real_escape_string($_POST['Phone']);
+		$password = $conn->real_escape_string($_POST['Password']);
 
 		$sql = "SELECT * FROM user WHERE email='$email'";
 
@@ -23,8 +23,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 			$_SESSION['message'] = "An account with that email already exists";
 		} else{
 			$_SESSION['email'] = $email;
-			$sql = "INSERT INTO user(email, firstName, lastName, phoneNumber, password) VALUES('$email', '$first_name', '$last_name', '$phone_num', '$password')";
-			if (mysqli_query($con, $sql)){
+			$sql = "INSERT INTO user(email, firstname, lastname, phonenum, password) VALUES('$email', '$first_name', '$last_name', '$phone_num', '$password')";
+			if (mysqli_query($conn, $sql)){
 				$_SESSION['message'] = "Registration successful";
 				header("location: index.html");
 				$to = "$email";
@@ -33,9 +33,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 				$from = "safe@safely.com";
 				$headers = "From:" . $from;
 
-				mail($to,$subject,$message,$headers)
-
-			} else{
+				mail($to,$subject,$message,$headers);
+			}
+			 else{
 				$_SESSION['message'] = "Please try again";
 			}
 		}
@@ -43,28 +43,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 		$_SESSION['message'] = "The passwords you have entered do not match";
 	}
 
+
 	
 }
 
 
-/*
-if ($email =='' || $first_name =='' || $last_name=='' || $phone_num==''|| $password==''){
-	echo 'Please fill in all the missing fields.';
-} else{
-	//require_once()
-	$sql = "SELECT * FROM user WHERE email='$email'";
-
-	$emailCheck = mysqli_fetch_array(mysqli_query($con, $sql));
-
-	if(isset($emailCheck)){
-		echo ' Email already exist';
-	} else{
-		$sql = "INSERT INTO user(email, firstName, lastName, phoneNumber, password) VALUES('$email', '$first_name', '$last_name', '$phone_num', '$password')";
-		if (mysqli_query($con, $sql)){
-			echo 'Registration successful';
-		} else{
-			echo 'Please try again!';
-		}
-	}
-} */
 ?>
